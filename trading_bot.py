298c9cdd8299e8f27da99e154def71929f0571c0
AarohanMahanta <aarohanmahanta@gmail.com>
2026-03-04 18:20:28 +0530

    feat: add alpaca credentials to trading_bot.py

--- trading_bot.py
+++ trading_bot.py
@@ -3,7 +3,15 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime
+import os
+from dotenv import load_dotenv
 
-API_KEY = ""
-API_SECRET = ""
-BASE_URL = ""
+API_KEY = os.get("ALPACA_API_KEY")
+API_SECRET = "ALPACA_API_SECRET"
+BASE_URL = "ALPACA_BASE_URL"
+
+ALPACA_CREDS = {
+    "API_KEY":API_KEY,
+    "API_SECRET":API_SECRET,
+    "PAPER": True
+}
