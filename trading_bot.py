c55e92fb84a6a7cb2222eed8170b30123b6e52b0
AarohanMahanta <aarohanmahanta@gmail.com>
2026-03-05 15:44:21 +0530

    faet: add imports for pytorch and transformers

--- trading_bot.py
+++ trading_bot.py
@@ -3,7 +3,9 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import os
+import torch
 from dotenv import load_dotenv
 from alpaca_trade_api import REST
 from timedelta import Timedelta
