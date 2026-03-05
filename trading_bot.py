7b5ccdd3b814628997a3d8d0a4bf877aa667f5e0
AarohanMahanta <aarohanmahanta@gmail.com>
2026-03-05 15:05:47 +0530

    feat: add get news method to retrieve news

--- trading_bot.py
+++ trading_bot.py
@@ -5,6 +5,8 @@ from lumibot.traders import Trader
 from datetime import datetime
 import os
 from dotenv import load_dotenv
+from alpaca_trade_api import REST
+from timedelta import Timedelta
 
 load_dotenv()
 
@@ -24,22 +26,38 @@ class MLTrader(Strategy):
         self.sleeptime = "24H"
         self.last_trade = None
         self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url=API_KEY, secret_key=API_SECRET, key_id=BASE_URL)
 
     def position_sizing(self):
         cash = self.get_cash()
         last_price = self.get_last_price(self.symbol)
         quantity = round(cash * self.cash_at_risk / last_price)
         return cash, last_price, quantity
+    
+    def get_dates(self):
+        today = self.get_datetime()
+        three_days_prior = today - Timedelta(days=3)
+        return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
+
+    def get_news(self):
+        today, three_days_prior = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol, start=three_days_prior, end=today)
+        news = [ev.__dict__["_raw"]["headline"] for ev in news]
+        return news
 
     def on_trading_iteration(self):
         cash, last_price, quantity = self.position_sizing ()
         if cash > last_price:
             if self.last_trade == None:
+                news = self.get_news()
+                print(news)
                 order = self.create_order(
                     self.symbol,
                     quantity,
                     "buy",
-                    type="market"
+                    type="bracket",
+                    take_profit_price=last_price*1.2,
+                    stop_limit_price=last_price*0.95
                 )
                 self.submit_order(order)
                 self.last_trade = "buy"
