3cb22c590dc332c5eaf9460e58bab029e24d9add
AarohanMahanta <aarohanmahanta@gmail.com>
2026-03-05 09:58:07 +0530

    feat: add trading strategy with start and end date

--- trading_bot.py
+++ trading_bot.py
@@ -6,12 +6,41 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 
-API_KEY = os.get("ALPACA_API_KEY")
-API_SECRET = "ALPACA_API_SECRET"
-BASE_URL = "ALPACA_BASE_URL"
+load_dotenv()
+
+API_KEY = os.getenv("ALPACA_API_KEY")
+API_SECRET = os.getenv("ALPACA_API_SECRET")
+BASE_URL = os.getenv("ALPACA_BASE_URL")
 
 ALPACA_CREDS = {
     "API_KEY":API_KEY,
     "API_SECRET":API_SECRET,
     "PAPER": True
 }
+
+class MLTrader(Strategy):
+    def initialize(self, symbol:str="SPY") :
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+    def on_trading_iteration(self):
+        if self.last_trade == None:
+            order = self.create_order(
+                self.symbol,
+                10,
+                "buy",
+                type="market"
+            )
+            self.submit_order(order)
+            self.last_trade = "buy"
+
+start_date = datetime(2023, 12, 15)
+end_date = datetime(2023, 12, 31)
+broker = Alpaca(ALPACA_CREDS)
+strategy = MLTrader(name='mlstrat', broker = broker, parameters={})
+strategy.backtest(
+    YahooDataBacktesting,
+    start_date,
+    end_date,
+    parameters={"symbol":"SPY"}
+)
